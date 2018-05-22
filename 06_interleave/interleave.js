d7bc1614cfca0231cb5d8bd0ffcb0b82578bc40d
Veronica Smith <veronica.smith2009@yahoo.com>
2018-05-22 15:36:06 -0400

    complete

--- 06_interleave/interleave.js
+++ 06_interleave/interleave.js
@@ -1,4 +1,19 @@
 function interleave() {
-  let args = [].slice.call(arguments);
-  return args;
+  let args = [...arguments],
+    res = '';
+
+  function loopingThru(arr, start) {
+    let newArr = [];
+
+    if (arr.length) {
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i][0]) {
+          start += arr[i][0];
+          newArr.push(arr[i].slice(1));
+        }
+      }
+      return loopingThru(newArr, start);
+    } else return start;
+  }
+  return loopingThru(args, res);
 }
